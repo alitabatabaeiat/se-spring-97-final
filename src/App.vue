fa9bfb212451bff5356cad02a6c5dc0543db6813
Ali Tabatabaei <a.tabatabaei97@gmail.com>
2018-05-29 19:18:07 +0430

    search field added + some improvement in goods card

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,15 @@
     <v-toolbar>
       <v-toolbar-title class="title">{{title}}</v-toolbar-title>
       <v-spacer></v-spacer>
+      <v-text-field
+        id="testing"
+        name="input-1"
+        label="جستجو ..."
+        v-show="searchField"
+      ></v-text-field>
+      <v-btn icon @click.native.stop="searchField=!searchField">
+        <v-icon>search</v-icon>
+      </v-btn>
       <v-toolbar-items class="items">
         <v-btn flat>
           <span>{{cart}}</span>
@@ -18,60 +27,29 @@
 
       </v-toolbar-items>
     </v-toolbar>
-    <v-content style="direction: rtl">
-      <v-container fluid>
+    <v-content style="direction: rtl" class="pa-3">
+      <v-container fluid grid-list-sm>
         <v-carousel hide-controls>
-        <v-carousel-item v-for="(item,i) in carousel" :src="item.src" :key="i"></v-carousel-item>
+          <v-carousel-item v-for="(item,i) in carousel" :src="item.src" :key="i"></v-carousel-item>
         </v-carousel>
 
         <v-divider class="my-4 mx-auto" style="width: 80%;"></v-divider>
 
         <v-subheader style="font-size: 28px; color: #000; margin: -10px 0 10px;">محصولات ما</v-subheader>
-        <v-layout>
-          <v-flex xs12 sm4 class="pl-3">
-            <v-card>
-              <v-card-media src="/static/1.jpg" height="200px">
-              </v-card-media>
-              <v-card-title primary-title>
-                <div>
-                  <h3>آبمیوه</h3>
-                </div>
-              </v-card-title>
-              <v-card-actions style="direction: ltr">
-                <v-btn flat :color="color">مشاهده</v-btn>
-                <v-btn flat :color="color">خرید</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-flex>
-          <v-flex xs12 sm4 class="pl-3">
-            <v-card>
-              <v-card-media src="/static/2.jpg" height="200px">
-              </v-card-media>
-              <v-card-title primary-title>
-                <div>
-                  <h3>کفش ورزشی</h3>
-                </div>
-              </v-card-title>
-              <v-card-actions style="direction: ltr">
-                <v-btn flat :color="color">مشاهده</v-btn>
-                <v-btn flat :color="color">خرید</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-flex>
-          <v-flex xs12 sm4>
-            <v-card>
-              <v-card-media src="/static/3.jpg" height="200px">
-              </v-card-media>
-              <v-card-title primary-title>
-                <div>
-                  <h3>لپ تاپ</h3>
-                </div>
-              </v-card-title>
-              <v-card-actions style="direction: ltr">
-                <v-btn flat :color="color">مشاهده</v-btn>
-                <v-btn flat :color="color">خرید</v-btn>
-              </v-card-actions>
-            </v-card>
+        <v-layout row wrap>
+          <v-flex xs12 sm3></v-flex>
+          <v-flex xs12 sm9>
+            <v-layout row wrap>
+              <v-flex xs12 sm4 v-for="(g,i) in goods" :key="i" class="pr-3 pb-3">
+                <v-card>
+                  <v-card-media :src="g.img" height="200px">
+                  </v-card-media>
+                  <v-card-title primary-title>
+                    <GoodsDetail :detail="g.detail"/>
+                  </v-card-title>
+                </v-card>
+              </v-flex>
+            </v-layout>
           </v-flex>
         </v-layout>
       </v-container>
@@ -80,6 +58,7 @@
 </template>
 
 <script>
+  import GoodsDetail from './components/GoodsDetail'
 
   export default {
     data() {
@@ -88,6 +67,7 @@
         fullName: 'سید علی طباطبایی',
         cart: 'سبد خرید',
         color: 'teal',
+        searchField: false,
         carousel: [
           {
             src: '/static/1.jpg'
@@ -98,14 +78,89 @@
           {
             src: '/static/3.jpg'
           }
+        ],
+        goods: [
+          {
+            img: '/static/3.jpg',
+            detail: {
+              name: 'لپ تاپ',
+              supplier: 'سید علی طباطبایی',
+              numOfVotes: '۲۲',
+              star: '۳.۴',
+              price: '۱۲۰۰۰'
+            }
+          },
+          {
+            img: '/static/1.jpg',
+            detail: {
+              name: 'آبمیوه',
+              supplier: 'سید علی طباطبایی',
+              numOfVotes: '۳۲',
+              star: '۴.۱',
+              price: '۱۲۰۲۰۰'
+            }
+          },
+          {
+            img: '/static/2.jpg',
+            detail: {
+              name: 'کفش ورزشی',
+              supplier: 'سید علی طباطبایی',
+              numOfVotes: '۱',
+              star: '۰.۳',
+              price: '۱۲۰'
+            }
+          }, {
+            img: '/static/2.jpg',
+            detail: {
+              name: 'کفش ورزشی',
+              supplier: 'سید علی طباطبایی',
+              numOfVotes: '۱',
+              star: '۱.۳',
+              price: '۱۲۰'
+            }
+          },
+          {
+            img: '/static/3.jpg',
+            detail: {
+              name: 'لپ تاپ',
+              supplier: 'سید علی طباطبایی',
+              numOfVotes: '۲۲',
+              star: '۲.۳',
+              price: '۱۲۰۰۰'
+            }
+          }
         ]
       }
     },
     name: 'App',
+    components: {
+      GoodsDetail
+    }
   }
 </script>
 
-<style>
+<style lang="scss">
+
+  .input-group.input-group--text-field {
+    direction: rtl;
+    label {
+      text-align: right;
+      max-width: 100%;
+      -webkit-transform-origin: top right;
+      -moz-transform-origin: top right;
+      -ms-transform-origin: top right;
+      -o-transform-origin: top right;
+      transform-origin: top right;
+      padding-right: 5px;
+    }
+
+    input {
+    }
+  }
+
+  .card {
+    cursor: pointer;
+  }
 
   .title {
     color: #008080;
---
+++ src/components/GoodsDetail.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <h2>{{detail.name}}</h2>
+    <h4>- تامین شده توسط <span class="supplierName">{{detail.supplier}}</span></h4>
+    <v-btn :color="colors[Math.floor(parseFloat(mapFaNumToEngNum(detail.star)))]" dark>
+      <v-icon style="margin-left: 3px">stars</v-icon>
+      <span>{{detail.star}}</span>
+    </v-btn>
+    <span>از {{detail.numOfVotes}} رای</span>
+    <h3>{{detail.price}} تومان</h3>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['detail'],
+    data() {
+      return {
+        colors: ['red darken-3', 'deep-orange darken-1', 'yellow darken-2', 'light-green darken-1', 'green darken-1', 'green darken-1'],
+        // color:
+      }
+    },
+    methods: {
+      mapFaNumToEngNum: (numStr) => {
+        numStr = numStr + "";
+        const faNums = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
+        for (let i = 0; i < numStr.length; i++) {
+          for (let j = 0; j < faNums.length; j++) {
+            if (numStr[i] === faNums[j]) {
+              numStr = numStr.replaceAt(i, j + '');
+              break;
+            }
+          }
+        }
+        return numStr;
+      }
+    }
+  }
+
+  String.prototype.replaceAt = function (index, replacement) {
+    return this.substr(0, index) + replacement + this.substr(index + replacement.length);
+  };
+  // const
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+
+  h4 {
+    font-weight: 100;
+    color: #555;
+
+    span.supplierName {
+      font-weight: 700;
+      color: teal;
+    }
+  }
+
+  .btn {
+    min-width: 0;
+    height: 30px;
+    margin-right: 0;
+
+    .btn__content {
+      padding: 0 5px;
+    }
+  }
+
+</style>
